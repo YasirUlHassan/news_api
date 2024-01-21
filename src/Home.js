5989a11506bf1258efd645b79a97d7d689adc649
YasirUlHassan <yasirmath2002@gmail.com>
2024-01-21 15:40:39 +0530

    new

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,42 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
-import News from "./News";
+// import { useEffect, useState } from "react";
+// import News from "./News";
 
 export default function Home() {
-    const [category, setCategory] = useState("india");
-    const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    // const [category, setCategory] = useState("india");
+    // const [articles, setArticles] = useState([]);
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState(null);
 
-    const current = new Date();
-    const month = (current.getMonth() + 1).toString().padStart(2, '0');
-    const date = `${current.getFullYear()}-${month}-${current.getDate() - 1}`;
+    // const current = new Date();
+    // const month = (current.getMonth() + 1).toString().padStart(2, '0');
+    // const date = `${current.getFullYear()}-${month}-${current.getDate() - 1}`;
 
-    useEffect(() => {
-        setLoading(true);
+    // useEffect(() => {
+    //     setLoading(true);
 
-        fetch(`https://newsapi.org/v2/everything?q=${category}&from=${date}&language=en&sortBy=publishedAt&apiKey=f6c1649e74304d65a3a369a7bba759bf`)
-            .then((response) => response.json())
-            .then((news) => {
-                setArticles(news.articles);
-                setLoading(false);
-            })
-            .catch((err) => {
-                setError(err);
-                setLoading(false);
-            });
-    }, [category]);
+    //     fetch(`https://newsapi.org/v2/everything?q=${category}&from=${date}&language=en&sortBy=publishedAt&apiKey=f6c1649e74304d65a3a369a7bba759bf`)
+    //         .then((response) => response.json())
+    //         .then((news) => {
+    //             setArticles(news.articles);
+    //             setLoading(false);
+    //         })
+    //         .catch((err) => {
+    //             setError(err);
+    //             setLoading(false);
+    //         });
+    // }, [category]);
     return (
         <div className="container">
             <header className="header">
                 <h1>DailySortFeed</h1>
                 <div className='header-right'>
                     <form>
-                        <input type="text" id="search" name="search"
-                            className='searchbox' onChange={(event) => {
-                                const value = event.target.value.trim();
-                                setCategory(value === "" ? "india" : value);
-                            }}
+                        <input type="text" id="search" name="search" className='searchbox' 
+                            // onChange={(event) => {
+                            //     const value = event.target.value.trim();
+                            //     setCategory(value === "" ? "india" : value);
+                            // }}
                             placeholder="Search News"
                         />
                     </form>
@@ -44,7 +44,8 @@ export default function Home() {
             </header>
 
             <section className="news">
-                <div className="loading-error">
+                hello
+                {/* <div className="loading-error">
                     {loading && <p>Loading...</p>}
                     {error && <p>Error: {error.message}</p>}
                 </div>
@@ -60,7 +61,7 @@ export default function Home() {
                             ))}
                         </div>
                     </>
-                )}
+                )} */}
             </section>
         </div>
     )
